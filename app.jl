68f63c47505a387230b54949ff1312de647dc03e
Matias Borghi <borghi.matias@gmail.com>
2021-06-20 22:12:44 -0300

    add more graphics to dash app

--- app.jl
+++ app.jl
@@ -1,23 +1,39 @@
 using Dash, DashHtmlComponents, DashCoreComponents;
 
+PORT = 1234
+if !("PORT" in keys(ENV))
+    println("PORT not set. Setting to default ", PORT)
+    ENV["PORT"] = PORT
+end
+
 app = dash();
 
 app.layout = html_div() do
     dcc_dropdown(
-        id = "dropdown",
-        options = [
+        id="dropdown",
+        options=[
             Dict("label" => "Los Angeles", "value" => "LA"),
             Dict("label" => "New York City", "value" => "NYC"),
             Dict("label" => "Montreal", "value" => "MTL")                
         ],
-        value = "LA"),
-    html_div(id = "display-value")
+        value="LA"),
+    html_div(id="display-value"),
+    dcc_graph(
+            id="example-graph",
+            figure=(
+                data = [
+                    (x = [1, 2, 3], y = [4, 1, 2], type = "bar", name = "SF"),
+                    (x = [1, 2, 3], y = [2, 4, 5], type = "bar", name = "Montréal"),
+                ],
+                layout = (title = "Dash Data Visualization",)
+            )
+        )
 end;
 
 callback!(app,
     Output("display-value", "children"),
     Input("dropdown", "value")) do value 
-        "You've entered $(value)"
+    "You've entered $(value)"
 end;
 
 # the default Dash port is 8050, but for Heroku deployments, it's
